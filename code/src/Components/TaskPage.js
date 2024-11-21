db257ca31e3a2e31c93620e3bb4b5497a63a0359
Kelistrah <emily.e.scott1@maine.edu>
2024-11-21 09:38:15 -0500

    Mid-work commit. Added TaskLogic, Task object, and started adding states to Modal

--- code/src/Components/TaskPage.js
+++ code/src/Components/TaskPage.js
@@ -1,28 +1,40 @@
-import React, {useEffect } from "react";
+import React, {useEffect, useState } from "react";
 import logo from "./../logo.svg";
 import logo2 from "./../logo2.svg";
 import Award from "./../Award.svg";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { auth } from '../Components/firebase';
-
-import { Button, Col } from "react-bootstrap";
+import { auth } from './firebase';
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { editTask } from './TaskLogic'
 import "./../App.css";
 
-function App() {
+function TaskPage() {
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [task, setTask] = useState("");
+  const [type, setType] = useState("");
+  const [dueDate, setDueDate] = useState(Date);
+  const [dueTime, setDueTime] = useState(Date);
+  const [isRepeat, setIsRepeat] = useState(Date);
+  const [isComplete, setIsComplete] = useState(false);
+  const TASKTYPE = ["Healthy Eating", "Rest", "Knowledge", "Social", "Tidyness", "Mental"]
 
+  function updateType(event) {
+    setType(event.target.value);
+}
+  
   // Function to open the modal
-  const openModal = () => {
-    const modal = document.getElementById("myModal");
+  const openEditTask = () => {
+    const modal = document.getElementById("editTask");
     if (modal) {
       modal.style.display = "block";
     }
   };
 
   // Function to close the modal
-  const closeModal = () => {
-    const modal = document.getElementById("myModal");
+  const closeEditTask = () => {
+    const modal = document.getElementById("editTask");
     if (modal) {
       modal.style.display = "none";
     }
@@ -244,7 +256,7 @@ function App() {
           </div>
           <Button
             id="newTaskButton"
-            onClick={openModal}
+            onClick={openEditTask}
             style={{
               marginTop: "2%",
               marginRight: "5%",
@@ -351,14 +363,72 @@ function App() {
       </div>
       <div className="App-background">{}</div>
 
-      <div id="myModal" className="modal">
+      <div id="editTask" className="modal">
         <div className="modal-content">
-          <Button onClick={closeModal}>Close</Button>
-          <p>Some text in the Modal..</p>
+          <Button onClick={closeEditTask}>Close</Button>
+          <Form.Group controlId="formTitle" as={Row}>
+                <Form.Label column sm={2}>
+                    Title:
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        value={title}
+                        onChange={(
+                            event,
+                        ) => {
+                            setTitle(event.target.value);
+                        }}
+                    />
+                </Col>
+            </Form.Group>
+            <Form.Group controlId="formDescription" as={Row}>
+                <Form.Label column sm={2}>
+                    Description:
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        size="lg"
+                        as="textarea"
+                        rows={5}
+                        cols={50}
+                        type="text"
+                        value={task}
+                        onChange={(
+                            event,
+                        ) => {
+                            setTask(event.target.value);
+                        }}
+                    />
+                </Col>
+            </Form.Group>
+            <Form.Group controlId="tasktype">
+                <Form.Label>Task Type:</Form.Label>
+                <Form.Select value={type} onChange={updateType}>
+                  { TASKTYPE.map((type) =>
+                    <option key={type} value={type}>{type}</option>
+                  )}
+                </Form.Select>
+            </Form.Group>
+            <Form.Group controlId="formTitle" as={Row}>
+                <Form.Label column sm={2}>
+                    Date:
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        value={dueDate}
+                        placeholder={Date.now()}
+                        onChange={(
+                            event,
+                        ) => {
+                            setDueDate(event.target.value);
+                        }}
+                    />
+                </Col>
+            </Form.Group>
         </div>
       </div>
     </>
   );
 }
 
-export default App;
+export default TaskPage;
---
+++ code/src/Interfaces/Task.js
@@ -0,0 +1,10 @@
+const Task = {
+    title: "",
+    task: "",
+    type: "",
+    dueDate: Date,
+    dueTime: Date,
+    isRepeat: Date,
+    isComplete: false
+}
+export default Task;
